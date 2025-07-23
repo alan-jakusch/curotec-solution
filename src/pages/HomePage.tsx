76bbacead49ddfff63417dc798b3f7eff0c4e5af
Alan Jakusch <alanjakusch@Alans-MacBook-Air.local>
2025-07-23 13:35:43 -0300

    create home page and organize router

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,8 @@
+export default function HomePage() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <h1 className="text-4xl font-bold mb-4">Welcome to the Home Page!</h1>
+      <p className="text-lg">This is the home page of the project.</p>
+    </div>
+  );
+} 
